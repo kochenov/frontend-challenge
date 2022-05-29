acb7ee50758c866a17229a91dae3979c226eed2f
kochenov <kochenof@gmail.com>
2022-05-29 11:04:54 +0300

    Изменил значение publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: "/frontend-challenge/",
   // devServer: {
   //   proxy: "https://api.thecatapi.com",
   //   //   //secure: false,
